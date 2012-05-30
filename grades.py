b61161cd17ca72330ab29eda49e0f1b94d4e7138
Quentin Pleplé <quentin.pleple@gmail.com>
2012-05-29 18:00:14 -0700

    added labels

--- grades.py
+++ grades.py
@@ -12,11 +12,11 @@ from gradesource import Gradesource
 import utils
 
 def importQuiz(moodleClassID, moodleColumn, assessmentId):
-  moodle = Moodle('qpleple', getpass(), moodleClassID)
+  moodle = Moodle('qpleple', getpass('Moodle password? '), moodleClassID)
   nameToScore = moodle.quizGrades(moodleColumn)
   utils.check("Moodle name -> score: ", nameToScore)
 
-  gradesource = Gradesource('quentin', getpass())
+  gradesource = Gradesource('quentin', getpass('Gradesource password? '))
   gradesource.importScores(nameToScore, assessmentId)
 
 def importParticipation(gradesource, assessmentId, date):
@@ -27,12 +27,12 @@ def importParticipation(gradesource, assessmentId, date):
   nameToScore = dict([(name, 1) for name in names])
   utils.check("Nname to score: ", nameToScore)
   
-  gradesource = Gradesource('qpleple1', getpass())
+  gradesource = Gradesource('quentin', getpass('Gradesource password? '))
   gradesource.importScores(nameToScore, assessmentId)
   
 
 # moodle = Moodle('qpleple', getpass(), 155)
 # nameToScore = moodle.quizGrades(9)
 
-importQuiz(175, 5, 420001)
+importQuiz(175, 6, 420002)
 # importParticipation('11-03-01', 416781)
